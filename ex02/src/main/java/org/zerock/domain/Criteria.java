3d5d9cede5caf966ad38748384ca1c679cd57a61
crunch28 <dh89168916@naver.com>
2019-11-08 11:50:00 +0900

    댓글 달기전 게시판

---
+++ ex02/src/main/java/org/zerock/domain/Criteria.java
@@ -0,0 +1,44 @@
+package org.zerock.domain;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Criteria {
+
+	private int pageNum;
+	private int amount;
+
+	private String type;
+	private String keyword;
+
+	public Criteria() {
+		this(1, 10);
+	}
+
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+
+	// 검색조건(T, W, C)을 배열로 처리하기 위해
+	public String[] getTypeArr() {
+		return type == null ? new String[] {} : type.split("");
+	}
+
+	//get 방식으로 데이터를 넘길때 사용
+	public String getListLink() {
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+				.queryParam("pageNum", this.pageNum)
+				.queryParam("amount", this.getAmount())
+				.queryParam("type", this.getType())
+				.queryParam("keyword", this.getKeyword());
+		
+		return builder.toUriString();
+	}
+}
